10989d675cc443bf1bbd6c82a95826d5f6fa5dc4
kapil_shrivastava <shrivastavakapil2000@yahoo.com>
2015-12-02 23:17:21 -0500

    kafka examples

---
+++ kafka-examples/SimpleCounter/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home/
+mvn install
+java -cp target/uber-SimpleCounter-1.0-SNAPSHOT.jar com.shapira.examples.producer.simplecounter.SimpleCounter localhost:9092 first new async 500 10
---
+++ kafka-examples/SimpleCounter/run_params.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java -cp target/uber-SimpleCounter-1.0-SNAPSHOT.jar com.shapira.examples.producer.simplecounter.SimpleCounter "$@"
